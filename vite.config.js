fd99ece2618c5e31f06b0a6fd8a9f1a8eddf78da
Vishal Thapa <46962439+kratos0002@users.noreply.github.com>
2024-11-28 09:14:41 +0000

    added new vercel json

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+  base: '/',
+  server: {
+    historyApiFallback: true,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
     },
   },
 })
